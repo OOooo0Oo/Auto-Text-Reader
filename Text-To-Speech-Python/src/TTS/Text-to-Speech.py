1d8e7c6be38ad8e743b597baaf9e39808f845fcb
OooooDes <OooooDes@vt.edu>
2021-07-05 00:25:05 +0800

    python configuration

--- Text-To-Speech-Python/src/TTS/Text-to-Speech.py
+++ Text-To-Speech-Python/src/TTS/Text-to-Speech.py
@@ -1,4 +1,5 @@
-
+import pyaudio
+import wave
 from azure.cognitiveservices.speech import AudioDataStream, SpeechConfig, SpeechSynthesizer, SpeechSynthesisOutputFormat
 from azure.cognitiveservices.speech.audio import AudioOutputConfig
 speech_key, service_region = "6a6d33d49c9947558a3c424286c2550d", "westus"
