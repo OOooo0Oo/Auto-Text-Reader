43696c8c5cd1ef8ea73e732dcad4d705c9c247c9
OooooDes <OooooDes@vt.edu>
2021-07-04 23:10:21 +0800

    python file start

--- Text-To-Speech-Python/src/TTS/Text-to-Speech.py
+++ Text-To-Speech-Python/src/TTS/Text-to-Speech.py
@@ -1,3 +1,14 @@
+
 from azure.cognitiveservices.speech import AudioDataStream, SpeechConfig, SpeechSynthesizer, SpeechSynthesisOutputFormat
 from azure.cognitiveservices.speech.audio import AudioOutputConfig
+speech_key, service_region = "6a6d33d49c9947558a3c424286c2550d", "westus"
 
+speech_config = SpeechConfig(subscription=speech_key, region=service_region)
+audio_config = AudioOutputConfig(use_default_speaker=True)
+
+synthesizer = SpeechSynthesizer(speech_config=speech_config, audio_config = audio_config)
+print("Type some text that you want to speak...")
+text = input()
+synthesizer.speak_text_async(text)
+
+#audio_config = AudioOutputConfig(filename="path/to/write/file.wav")
