513bce17a2d428a7a7728983213846eb623c7d68
OooooDes <OooooDes@vt.edu>
2021-07-04 23:42:03 +0800

    python ssml

--- Text-To-Speech-Python/src/TTS/Text-to-Speech.py
+++ Text-To-Speech-Python/src/TTS/Text-to-Speech.py
@@ -5,10 +5,12 @@ speech_key, service_region = "6a6d33d49c9947558a3c424286c2550d", "westus"
 
 speech_config = SpeechConfig(subscription=speech_key, region=service_region)
 audio_config = AudioOutputConfig(use_default_speaker=True)
+ssml_string = open("ssml.xml", "r",encoding='UTF-8').read()
+
 
 synthesizer = SpeechSynthesizer(speech_config=speech_config, audio_config = audio_config)
-print("Type some text that you want to speak...")
-text = input()
-synthesizer.speak_text_async(text)
+#print("Type some text that you want to speak...")
+#text = input()
+synthesizer.speak_ssml_async(ssml_string)
 
 #audio_config = AudioOutputConfig(filename="path/to/write/file.wav")
